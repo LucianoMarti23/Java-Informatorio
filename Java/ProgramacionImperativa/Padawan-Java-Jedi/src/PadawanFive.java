d22131c170c7b8320c2a258472afe4c4eb1fef19
Luciano <luciano.marti12@hotmail.com>
2023-09-24 22:31:40 -0300

    Padawan Five

---
+++ Java/ProgramacionImperativa/Padawan-Java-Jedi/src/PadawanFive.java
@@ -0,0 +1,20 @@
+public class PadawanFive {
+    public static void main(String[] args){
+
+
+        //Conversión a kilogramos: Guarde en un espacio de memoria sus libras y luego, en
+        //otro espacio de memoria realice la conversión de esas libras a kilogramos. Utilice
+        //una constante de los valores que no van a cambiar en esta conversión. Muestre el
+        //resultado por pantalla
+
+
+        final double kilogramo = 2.20462;
+
+
+        int libras = 10;
+
+        System.out.println("La cantidad de kilos dada las libras son :  " + (int)(libras/kilogramo));
+
+
+    }
+}
