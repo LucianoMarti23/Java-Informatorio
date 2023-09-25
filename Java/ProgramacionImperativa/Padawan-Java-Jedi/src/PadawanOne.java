452120cfe8160885f55258116a5f7bda6c4763d3
Luciano <luciano.marti12@hotmail.com>
2023-09-24 21:34:38 -0300

    Padawan One
    Java Podawan1

---
+++ Java/ProgramacionImperativa/Padawan-Java-Jedi/src/PadawanOne.java
@@ -0,0 +1,34 @@
+
+public class PadawanOne{
+    public static void main(String[] args) {
+        int numUno = 0;
+
+            // Se le solicita que guarde un número en un espacio de memoria e
+            //incremente dicho número 20 veces sin usar asignación. ¿Que operador podría usar
+            //para hacerlo?
+
+            // EL operador de Incremento.
+           numUno++;
+           numUno++;
+           numUno++;
+           numUno++;
+           numUno++;
+           numUno++;
+           numUno++;
+           numUno++;
+           numUno++;
+           numUno++;
+           numUno++;
+           numUno++;
+           numUno++;
+           numUno++;
+           numUno++;
+           numUno++;
+           numUno++;
+           numUno++;
+           numUno++;
+           numUno++;
+           System.out.println(numUno);
+           
+        }
+    }
