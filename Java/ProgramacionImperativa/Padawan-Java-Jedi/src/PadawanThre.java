3f94e44a7e74012cb5427f062acdd8c2bb9ee6ab
Luciano <luciano.marti12@hotmail.com>
2023-09-24 22:02:44 -0300

    Padawan Thre

---
+++ Java/ProgramacionImperativa/Padawan-Java-Jedi/src/PadawanThre.java
@@ -0,0 +1,30 @@
+public class PadawanThre {
+    public static void main(String[] args){
+        // Caracteres : Guarde en distintos espacios de memoria los caracteres de su nombre
+        // en unicode y luego imprimirlos por pantalla. ¿Por qué no puede usar "" en el char?
+        // ¿Qué es unicode?
+           
+
+
+        // Unicode es un sistema de codificación de caracteres 
+        //utilizado por los equipos informáticos para el almacenamiento y el intercambio de datos en formato de texto
+
+        byte l = 108;
+
+        byte u = 117;
+
+        byte c = 99;
+
+        byte i = 105;
+
+        byte a = 97;
+
+        byte n = 110;
+
+        byte o = 111;
+
+        System.out.println(" " + ((char) l) + ((char)  u ) + ((char) c) + ((char) i)+((char) a )+ ((char) n ) + ((char) o));
+
+
+    }
+}
