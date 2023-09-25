ee8856352947c68e67b5668f23d2c38e3f6d9a02
Luciano <luciano.marti12@hotmail.com>
2023-09-24 21:44:55 -0300

    PadawanTwo

---
+++ Java/ProgramacionImperativa/Padawan-Java-Jedi/src/PadawanTwo.java
@@ -0,0 +1,36 @@
+public class PadawanTwo {
+    public static void main(String[] args){
+
+        // Descontador: Se le solicita que guarde un número en un espacio de memoria y
+        //discretamente dicho número 20 veces sin usar asignación. ¿Que operador podría
+        //usar para hacerlo?
+
+        // EL operador de Decremento --
+
+        int numUno = 0;
+           numUno--;
+           numUno--;
+           numUno--;
+           numUno--;
+           numUno--;
+           numUno--;
+           numUno--;
+           numUno--;
+           numUno--;
+           numUno--;
+           numUno--;
+           numUno--;
+           numUno--;
+           numUno--;
+           numUno--;
+           numUno--;
+           numUno--;
+           numUno--;
+           numUno--;
+           numUno--;
+           System.out.println(numUno);
+
+
+    }
+    
+}
